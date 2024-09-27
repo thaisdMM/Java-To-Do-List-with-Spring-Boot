118d304092b6a08b3d042e06054387714078827a
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-09-27 18:48:42 +0200

    todo app Java

---
+++ src/main/java/io/github/thaisdmm/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,35 @@
+package io.github.thaisdmm.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/primeiraRota")
+//ROTA: http:/localhost:8080/ ---
+public class MinhaPrimeiraController {
+
+  /**
+   * Méto de acesso do HTTP
+   * GET - Buscar uma informação
+   * POST - Adicionar um dado/informação
+   * PUT - Alterar um dado/info
+   * DELET - Remover um dado
+   * PATCH - Alterar somente uma parte da info/dado
+   */
+
+   //Método (Funcionalidade) de uma classe
+
+   @GetMapping("/")
+   public String getMethodName(@RequestParam String param) {
+       return new String();
+   }
+   
+  public String primeiramensagem() {
+    return "Funcionou";
+  }
+  
+}
