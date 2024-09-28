f291a4d12ad4a62ec669e145faa8980c71b6aca9
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-09-28 11:57:48 +0200

    delete Get and Set

--- src/main/java/io/github/thaisdmm/todolist/user/UserModel.java
+++ src/main/java/io/github/thaisdmm/todolist/user/UserModel.java
@@ -5,30 +5,5 @@ public class UserModel {
   private String username;
   private String name;
   private String password;
-
-  // getters e setters
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-  
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-  public String getPassword() {
-    return password;
-  }
   
 }
