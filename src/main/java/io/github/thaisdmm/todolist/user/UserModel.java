f045741b62437e04d896a0a2278abc48dfe3fdf3
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-09-28 11:15:09 +0200

    change UserModel to private

--- src/main/java/io/github/thaisdmm/todolist/user/UserModel.java
+++ src/main/java/io/github/thaisdmm/todolist/user/UserModel.java
@@ -2,8 +2,8 @@ package io.github.thaisdmm.todolist.user;
 
 public class UserModel {
 
-  public String username;
-  public String name;
-  public String password;
+  private String username;
+  private String name;
+  private String password;
   
 }
