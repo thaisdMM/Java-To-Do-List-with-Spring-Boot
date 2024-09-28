fd945185d7bccf2cbb246d69fb2b66ef4d841925
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-09-28 10:06:58 +0200

    criando UserController

---
+++ src/main/java/io/github/thaisdmm/todolist/user/UserController.java
@@ -0,0 +1,27 @@
+package io.github.thaisdmm.todolist.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificar
+ * public
+ * private
+ * protected
+ */
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+/**
+ * Integer(int) numeros inteiros
+ * Date(data)
+ * void - nao tem um retorno do método
+ */
+
+  public void create() {
+    
+  }
+  
+}
