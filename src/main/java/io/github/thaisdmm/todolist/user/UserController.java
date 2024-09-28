e8a0a2c29ed5cea351d194589869be9d2f80fa5c
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-09-28 17:44:17 +0200

    add findByUsername
    adicionado para evitar o erro de estourar o terminal. Entretanto está dando código 200 de sucesso no ApiDog. Tem que concertar.

--- src/main/java/io/github/thaisdmm/todolist/user/UserController.java
+++ src/main/java/io/github/thaisdmm/todolist/user/UserController.java
@@ -23,6 +23,12 @@ public class UserController {
 
   @PostMapping("/")
   public UserModel create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if(user != null) {
+      System.out.println("Usuário já existe");
+      return null;
+    }
     var userCreated = this.userRepository.save(userModel);
     return userCreated;
   }
