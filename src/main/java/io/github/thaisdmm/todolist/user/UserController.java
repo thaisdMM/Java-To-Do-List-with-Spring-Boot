08203dd6a949ae2d0d77c490dcd05fb704b5c72a
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-09-28 17:59:55 +0200

    RespnseEntity created Bad_Request

--- src/main/java/io/github/thaisdmm/todolist/user/UserController.java
+++ src/main/java/io/github/thaisdmm/todolist/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -22,12 +24,14 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel userModel) {
+  public ResponseEntity create(@RequestBody UserModel userModel) {
     var user = this.userRepository.findByUsername(userModel.getUsername());
 
     if(user != null) {
       System.out.println("Usuário já existe");
-      return null;
+      //Retornar mensagem de erro
+      // Status Code - são os "numeros" dos status da requisição ApiDog - por padrão http tem satus code
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
     }
     var userCreated = this.userRepository.save(userModel);
     return userCreated;
