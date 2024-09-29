cc717e04ca4b0f38d99e561b39d5a0c324b8373a
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-09-29 14:58:17 +0200

    limited Char in the title

--- src/main/java/io/github/thaisdmm/todolist/task/TaskModel.java
+++ src/main/java/io/github/thaisdmm/todolist/task/TaskModel.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -29,11 +30,13 @@ public class TaskModel {
 
    private UUID id;
    private String description;
+
+   @Column(length = 50)
    private String title;
    private LocalDateTime startAt;
    private LocalDateTime endAt;
    private String priority;
-   
+
    private UUID idUser;
   
    @CreationTimestamp
