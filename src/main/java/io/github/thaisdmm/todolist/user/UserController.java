7fb1fb5ee89f3ffc062b87e61fce2995a92c1fdd
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-09-29 14:33:12 +0200

    criptografando a senha

--- src/main/java/io/github/thaisdmm/todolist/user/UserController.java
+++ src/main/java/io/github/thaisdmm/todolist/user/UserController.java
@@ -3,6 +3,9 @@ package io.github.thaisdmm.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +35,12 @@ public class UserController {
       // Status Code - são os "numeros" dos status da requisição ApiDog - por padrão http tem satus code
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
     }
+
+    var passwordHashred = BCrypt.withDefaults()
+      .hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordHashred);
+
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
