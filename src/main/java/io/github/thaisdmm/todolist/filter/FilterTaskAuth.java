ce4213df94c061f6fb9d78f7d836472a6df9644c
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-09-29 19:29:21 +0200

    tentendo resolver o problema do null
    com ajuda do chat GPT tentando corrigir o código

--- src/main/java/io/github/thaisdmm/todolist/filter/FilterTaskAuth.java
+++ src/main/java/io/github/thaisdmm/todolist/filter/FilterTaskAuth.java
@@ -2,6 +2,7 @@ package io.github.thaisdmm.todolist.filter;
 
 import java.io.IOException;
 
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,8 +14,11 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
-  @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request, 
+            @NonNull HttpServletResponse response, 
+            @NonNull FilterChain filterChain)
             throws ServletException, IOException {
               
 // Pegar a autenticação(usuário e senha)
