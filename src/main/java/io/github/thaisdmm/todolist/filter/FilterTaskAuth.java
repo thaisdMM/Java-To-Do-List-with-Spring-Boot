5c1b56851f61a18fa634f240999d9a4f39f43b80
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-03 15:14:54 +0200

    Validate if the user exists in the database
    If user exists: validate password
    If user does not exist: return error

--- src/main/java/io/github/thaisdmm/todolist/filter/FilterTaskAuth.java
+++ src/main/java/io/github/thaisdmm/todolist/filter/FilterTaskAuth.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.github.thaisdmm.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +16,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
 
   @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -32,20 +37,21 @@ public class FilterTaskAuth extends OncePerRequestFilter {
               String [] credentials = authString.split(":");
               String username = credentials[0];
               String password = credentials[1];
-              System.out.println("Authorization");
-              System.out.println(username);
-              System.out.println(password);
               
-
-
-
 // Validar usuário
 
+              var user = this.userRepository.findByUsername(username);
+              if(user == null) {
+                response.sendError(401, "Usuário sem autorização");
+              } else {
 // Validar Senha
 
 //Segue viagem
 
 filterChain.doFilter(request, response);
-  }
+
+}
+
+}
  
 }
