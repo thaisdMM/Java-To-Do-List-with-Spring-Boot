efc4eaa09e3dc8511cc8f223078ce3e31f4924c7
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-04 14:25:15 +0200

    created a feature for the user to list tasks

--- src/main/java/io/github/thaisdmm/todolist/task/TaskContreller.java
+++ src/main/java/io/github/thaisdmm/todolist/task/TaskContreller.java
@@ -8,12 +8,14 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.var;
 
 @RestController
 @RequestMapping("/tasks")
@@ -42,6 +44,12 @@ public class TaskContreller {
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(HttpStatus.OK).body(task);
   
-  
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list( HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+    return tasks;
   }
 }
