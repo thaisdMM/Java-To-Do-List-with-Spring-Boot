53ced776b738424fc3bb93f7af943c10f0f9f939
Thaís Moreira <thaisdmedeiros@gmail.com>
2024-10-05 17:00:00 +0200

    App reload with add devtools

--- src/main/java/io/github/thaisdmm/todolist/user/UserController.java
+++ src/main/java/io/github/thaisdmm/todolist/user/UserController.java
@@ -1,15 +1,14 @@
 package io.github.thaisdmm.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 /**
@@ -42,7 +41,7 @@ public class UserController {
     userModel.setPassword(passwordHashred);
 
     var userCreated = this.userRepository.save(userModel);
-    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    return ResponseEntity.status(HttpStatus.OK).body(userCreated);
   }
   
 }
